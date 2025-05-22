2523b7665013d1e197cc3352ef0c697c1c124fa2
Nidhi0152 <194421451+Nidhi0152@users.noreply.github.com>
2025-05-22 19:11:27 -0400

    adding IaC

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,7 +1,8 @@
 import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
 import * as pulumi from "@pulumi/pulumi";
-
+import * as dockerBuild from '@pulumi/docker-build'
+import * as containerinstance from '@pulumi/azure-native/containerinstance'
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config()
 const appPath = config.require('appPath')
@@ -16,7 +17,7 @@ const cpu = config.requireNumber('cpu')
 const memory = config.requireNumber('memory')
 
 // Create a resource group.
-const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`)
+const resourceGroup = new resources.ResourceGroup(`${prefixName}rg`)
 
 // Create the container registry.
 const registry = new containerregistry.Registry(`${prefixName}ACR`, {
@@ -39,6 +40,84 @@ const registryCredentials = containerregistry
       password: creds.passwords![0].value!,
     }
   })
-  
-export const acrServer = registry.loginServer
-export const acrUsername = registryCredentials.username
+
+//export const acrServer = registry.loginServer
+//export const acrUsername = registryCredentials.username
+
+// Define the container image for the service.
+const image = new dockerBuild.Image(`${prefixName}-image`, {
+  tags: [pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`],
+  context: { location: appPath },
+  dockerfile: { location: `${appPath}/Dockerfile` },
+  target: 'production',
+  platforms: ['linux/amd64', 'linux/arm64'],
+  push: true,
+  registries: [
+    {
+      address: registry.loginServer,
+      username: registryCredentials.username,
+      password: registryCredentials.password,
+    },
+  ],
+})
+
+// Create a container group in the Azure Container App service and make it publicly accessible.
+const containerGroup = new containerinstance.ContainerGroup(
+  `${prefixName}-container-group`,
+  {
+    resourceGroupName: resourceGroup.name,
+    osType: 'linux',
+    restartPolicy: 'always',
+    imageRegistryCredentials: [
+      {
+        server: registry.loginServer,
+        username: registryCredentials.username,
+        password: registryCredentials.password,
+      },
+    ],
+    containers: [
+      {
+        name: imageName,
+        image: image.ref,
+        ports: [
+          {
+            port: containerPort,
+            protocol: 'tcp',
+          },
+        ],
+        environmentVariables: [
+          {
+            name: 'PORT',
+            value: containerPort.toString(),
+          },
+          {
+            name: 'WEATHER_API_KEY',
+            value: '6117e4ec514f64368fa7affcdbcbb2a0',
+          },
+        ],
+        resources: {
+          requests: {
+            cpu: cpu,
+            memoryInGB: memory,
+          },
+        },
+      },
+    ],
+    ipAddress: {
+      type: containerinstance.ContainerGroupIpAddressType.Public,
+      dnsNameLabel: `${imageName}`,
+      ports: [
+        {
+          port: publicPort,
+          protocol: 'tcp',
+        },
+      ],
+    },
+  },
+)
+// Export the service's IP address, hostname, and fully-qualified URL.
+export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
+export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
+export const url = containerGroup.ipAddress.apply(
+  (addr) => `http://${addr!.fqdn!}:${containerPort}`,
+)
